e9cae8cb227e6f9f03455b075c84a112aea09da4
Rawcossio <raulcossio166183@gmail.com>
2025-03-13 17:34:13 -0500

    Se crea componente de comida en descuento y se agrega a la seccion de descuentos de la App NutriGo

--- src/components/EnDescuento.jsx
+++ src/components/EnDescuento.jsx
@@ -1,9 +1,15 @@
-function EnDescuento(){
+function EnDescuento({info}){
     return(
         <div className="ComidaDescuento">
-            <img src="" alt="" />
-            <h4>Ensalada</h4>
-            <button>dias restantes</button>
+            <section className="CartasDescuento">
+            <img src={info.img} alt={info.food} />
+            <h3>{info.off}</h3>
+            <p>off</p>
+            </section>
+            <section className="TituloBoton">
+            <h4>{info.food}</h4>
+            <button>{info.dias_restantes} dias restantes</button>
+            </section>
         </div>     
     )
 }
--- src/pages/HealthFood.jsx
+++ src/pages/HealthFood.jsx
@@ -4,7 +4,33 @@ import EncuentraComida from "../Components/EncuentraComida"
 import Header from "../Components/Header"
 
 const HealthFood=()=>{
-    let repetidos=[1,2,3,4]
+    let pedidos=[
+        {
+            img:"/ensalada_10.png",
+            off:"15%",
+            food:"Ensalada",
+            dias_restantes:"6"
+        },
+        {
+            img:"/ensalada_15.png",
+            off:"10%",
+            food:"Ensalada",
+            dias_restantes:"6"
+        },
+        {
+            img:"/ensalada_25.png",
+            off:"25%",
+            food:"Ensalada",
+            dias_restantes:"7"
+        },
+        {
+            img:"/ensalada_20.png",
+            off:"20%",
+            food:"Ensalada",
+            dias_restantes:"8"
+        }
+
+    ]
     return(
         <html>
         <body>
@@ -13,8 +39,8 @@ const HealthFood=()=>{
                 <EncuentraComida/>
             </section>
         <div className="tarjetas">
-            {repetidos.map((repetido)=>{
-                return<EnDescuento/>
+            {pedidos.map((pedido)=>{
+                return<EnDescuento info={pedido}/>
             })
             }
         </div>
