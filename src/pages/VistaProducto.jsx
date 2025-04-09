6b9349caa79c5d0d010abade2339795774fdf49a
Chala-java <chalajuandavid876@gmail.com>
2025-04-08 20:06:55 -0500

    Implementacion VistaProducto

--- src/pages/VistaProducto.jsx
+++ src/pages/VistaProducto.jsx
@@ -3,16 +3,16 @@ import './VistaProducto.css';
 function VistaProducto() {
   return (
     <section className="vista">
-      <div className="vista-contenido">
+      <div className="vista__contenido">
         
         {/* Header */}
-        <header className="vista-header">
-          <div className="logo-nutri">
-            <img src="/go.png" alt="Nutri Logo" className="logo" />
+        <header className="vista__header">
+          <div className="vista__logo-nutri">
             <h1>Nutri</h1>
+            <img src="/go.png" alt="Nutri Logo" className="vista__logo" />
           </div>
-          <div className="restaurante-info">
-            <img src="/restaurante-logo.png" alt="Restaurante" className="logo-restaurante" />
+          <div className="vista__restaurante-info">
+            <img src="/restaurante-logo.png" alt="Restaurante" className="vista__logo-restaurante" />
             <div>
               <h3>Sabor Élite <br /> (Medellín)</h3>
               <p>⭐ 4.5 (200 reseñas)</p>
@@ -21,37 +21,31 @@ function VistaProducto() {
         </header>
 
         {/* Imagen del Producto */}
-        <div className="vista-imagen">
+        <div className="vista__imagen">
           <img src="/carne.jpg" alt="Huevos rotos con boletus y trufa" />
         </div>
 
         {/* Descripción */}
-        <main className="vista-info">
-          <h1 className="titulo">Huevos rotos con boletus y trufa</h1>
-          <p className="ingredientes">Patatas, boletus, cebolleta, ajetes, trufa, cebollino, aceite de oliva, huevos</p>
-          <div className="precio">
-             <h2> 
-             $ 15.000 
-            </h2>
-             <h3> 
-             $20.000
-            </h3>
+        <main className="vista__info">
+          <h1 className="vista__titulo">Huevos rotos con boletus y trufa</h1>
+          <p className="vista__ingredientes">
+            patatas, boletus, cebolleta, manojito de ajetes, trufa, cebollino, aceite de oliva, 8 huevos
+          </p>
+          <div className="vista__precio">
+            <h2>$15.000</h2>
+            <h3>$20.000</h3>
           </div>
         </main>
 
-        {/* Botón */}
-        <section className='contenido-footer'>
-
-
-        <div class="quantity-selector">
-    <div class="quantity-btn" onclick="decreaseQuantity()">-</div>
-    <div class="quantity-display" id="quantity">1</div>
-    <div class="quantity-btn" onclick="increaseQuantity()">+</div>
-  </div>
-  
-  <button class="cart-btn">Ir al carrito</button>
-        </section>
-
+        {/* Footer */}
+        <footer className="vista__footer">
+          <div className="vista__cantidad">
+            <div className="vista__cantidad-btn">-</div>
+            <div className="vista__cantidad-display">1</div>
+            <div className="vista__cantidad-btn">+</div>
+          </div>
+          <button className="vista__cart-btn">Ir al carrito</button>
+        </footer>
 
       </div>
     </section>
