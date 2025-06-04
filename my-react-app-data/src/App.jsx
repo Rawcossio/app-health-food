affd0a422fa1c7a9ffefb231db490a8296a76f97
Rawcossio <raulcossio166183@gmail.com>
2025-06-04 12:55:36 -0500

    se modifica el componente restaurantes

---
+++ my-react-app-data/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import SearchPost from './components/SearchPost/SearchPost'
+import StaticComponent from './components/StaticComponent/StaticComponent'
+import UserList from './components/UserList/UserList'
+
+function App() {
+
+  return (
+    <>
+      <SearchPost />
+    </>
+  )
+}
+
+export default App
